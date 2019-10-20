ad79af8bbb50d48b211d009ec550ad74e4038e6f
bnguye14 <ngpbach@gmail.com>
2019-10-20 16:48:42 -0500

    first joint slider working

--- src/owi/script/owi_node.py
+++ src/owi/script/owi_node.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import sys
+import os
 import time
 import rospy
 from PySide2 import QtCore, QtWidgets, QtUiTools
@@ -7,42 +8,28 @@ import random
 from owi.msg import joint_state
 from owi.msg import position_cmd
 
-class MyWidget(QtWidgets.QWidget):
-    def __init__(self):
-        super(MyWidget, self).__init__()
+pub = rospy.Publisher("command", position_cmd, queue_size=10)
+ 
 
-        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo"]
-
-        self.button = QtWidgets.QPushButton("Click me!")
-        self.text = QtWidgets.QLabel("Hello World")
-        self.text.setAlignment(QtCore.Qt.AlignCenter)
-
-        self.layout = QtWidgets.QVBoxLayout()
-        self.layout.addWidget(self.text)
-        self.layout.addWidget(self.button)
-        self.setLayout(self.layout)
-
-        self.button.clicked.connect(self.magic)
-
-
-    def magic(self):
-        self.text.setText(random.choice(self.hello))
-
+def handler(value):
+    pub.publish((value,90,0,0,0))
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + " Joints state: %s", data.position)
 
 def main():    
-    # rospy.init_node("owi_node", anonymous=True)
-    # rospy.Subscriber("joint_state", joint_state, callback)    
-    # pub = rospy.Publisher("command", position_cmd, queue_size=10)
+    rospy.init_node("owi_node", anonymous=True)
+    rospy.Subscriber("joint_state", joint_state, callback)    
 
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QtWidgets.QApplication(sys.argv)
-    loader = QtUiTools.QUiLoader()
-    MainWindow = loader.load("./CtrlPanel.ui")
-    MainWindow.show()
+    uiFilename = os.path.join(os.path.dirname(__file__), "CtrlPanel.ui")
+    window = QtUiTools.QUiLoader().load(uiFilename)
 
+    slider0 = window.findChild(QtWidgets.QSpinBox, "spinBox_1")
+    slider0.valueChanged.connect(handler)
+
+    window.show()
 
     sys.exit(app.exec_())
 
