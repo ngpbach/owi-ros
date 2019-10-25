6b081b876745d2e5df8f4da9ef1229f3534c053a
bnguye14 <bnguye14@BNbuntuVM.trick.gov>
2019-10-25 17:45:07 -0500

    working on joint_state_publisher

--- src/owi/script/owi_node.py
+++ src/owi/script/owi_node.py
@@ -5,23 +5,38 @@ import time
 import rospy
 from PySide2 import QtCore, QtWidgets, QtUiTools
 import random
-from owi.msg import joint_state
-from owi.msg import position_cmd
+from sensor_msgs.msg import JointState
+import owi.msg
 
-pub = rospy.Publisher("command", position_cmd, queue_size=10)
+cmdPub = rospy.Publisher("command", owi.msg.position_cmd, queue_size=10)
+statePub = rospy.Publisher("owi_state_report", JointState, queue_size=10)
 textbox = 0 
+joint_names = [ "jointLink1",
+                "jointLink2",
+                "jointLink3",
+                "jointLink4",
+                "jointRgripper"]
 
 def handler(value):
-    pub.publish((value,90,0,0,0))
+    cmdPub.publish((value,90,0,0,0))
 
 def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + " Joints state: %s", data.position)
-    global textbox
+    rospy.loginfo(rospy.get_caller_id() + " joint angles: %s", data.position)
     textbox.setText("%d\t %d\t %d\t %d\t %d" % (data.position[0], data.position[1], data.position[2], data.position[3], data.position[4]))
 
+    state = JointState()
+    for i,name in enumerate(joint_names):
+        state.name.append(name)
+        state.position.append(data.position[i])
+        state.velocity.append(0)
+        state.effort.append(0)
+
+    statePub.publish(state)
+
 def main():    
+    global textbox
     rospy.init_node("owi_node", anonymous=True)
-    rospy.Subscriber("joint_state", joint_state, callback)    
+    rospy.Subscriber("state", owi.msg.joint_state, callback)
 
     QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_ShareOpenGLContexts)
     app = QtWidgets.QApplication(sys.argv)
@@ -31,7 +46,6 @@ def main():
     slider0 = window.findChild(QtWidgets.QSpinBox, "spinBox_1")
     slider0.valueChanged.connect(handler)
     
-    global textbox 
     textbox = window.findChild(QtWidgets.QLabel, "label_6")
     textbox.setText("hello")
 
