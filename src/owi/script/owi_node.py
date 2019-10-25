fc2b285e793375ed45e9ac562c032d2391dcfe86
bnguye14 <ngpbach@gmail.com>
2019-10-24 23:20:04 -0500

    updated urdf with collision

--- src/owi/script/owi_node.py
+++ src/owi/script/owi_node.py
@@ -9,13 +9,15 @@ from owi.msg import joint_state
 from owi.msg import position_cmd
 
 pub = rospy.Publisher("command", position_cmd, queue_size=10)
- 
+textbox = 0 
 
 def handler(value):
     pub.publish((value,90,0,0,0))
 
 def callback(data):
     rospy.loginfo(rospy.get_caller_id() + " Joints state: %s", data.position)
+    global textbox
+    textbox.setText("%d\t %d\t %d\t %d\t %d" % (data.position[0], data.position[1], data.position[2], data.position[3], data.position[4]))
 
 def main():    
     rospy.init_node("owi_node", anonymous=True)
@@ -28,6 +30,10 @@ def main():
 
     slider0 = window.findChild(QtWidgets.QSpinBox, "spinBox_1")
     slider0.valueChanged.connect(handler)
+    
+    global textbox 
+    textbox = window.findChild(QtWidgets.QLabel, "label_6")
+    textbox.setText("hello")
 
     window.show()
 
