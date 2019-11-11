ca080bfeefc523e25c82a5c292c45d43ac8516f6
bnguye14 <ngpbach@gmail.com>
2019-11-10 20:37:00 -0600

    robot following fake execution

--- src/owi/script/owi_moveit.py
+++ src/owi/script/owi_moveit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python2
 import sys
+import os
 import copy
 import rospy
 import moveit_commander
@@ -14,11 +15,17 @@ def plan_path():
     scene = moveit_commander.PlanningSceneInterface()
     group = moveit_commander.MoveGroupCommander("arm")
 
-    display_trajectory_publisher = rospy.Publisher("move_group/display_planned_path", moveit_msgs.msg.DisplayTrajectory, queue_size=10)
+    # display_trajectory_publisher = rospy.Publisher("move_group/display_planned_path", moveit_msgs.msg.DisplayTrajectory, queue_size=10)
     rospy.sleep(5)
 
-    group.set_random_target()
-    group.plan()
+    head_pose = geometry_msgs.msg.PoseStamped()
+    head_pose.header.frame_id = "base_link"
+    head_pose.pose.position = geometry_msgs.msg.Point(x=0.5, y=0.0, z=0.5)
+    head_pose.pose.orientation = geometry_msgs.msg.Quaternion(x=0.0, y=0.0, z=-0.5, w=0.5)
+    filename = os.path.join(os.path.dirname(__file__), "head.stl")
+    scene.add_mesh("head", head_pose, filename, size=(0.001,0.001,0.001))
+    # group.set_random_target()
+    # group.plan()
 
 if __name__=='__main__':
   try:
--- src/owi/script/owi_node.py
+++ src/owi/script/owi_node.py
@@ -15,7 +15,8 @@ class Panel():
     @classmethod
     def __init__(cls):
         rospy.init_node("owi_node", anonymous=True)
-        rospy.Subscriber("state", owi.msg.joint_state, cls.callback)
+        rospy.Subscriber("state", owi.msg.joint_state, cls.report)
+        rospy.Subscriber("/move_group/fake_controller_joint_states", JointState,cls.execute)
         cls.cmdPub = rospy.Publisher("command", owi.msg.position_cmd, queue_size=10)
         cls.statePub = rospy.Publisher("owi_state_report", JointState, queue_size=10)
 
@@ -40,16 +41,16 @@ class Panel():
         cls.spinbox5 = window.findChild(QtWidgets.QSpinBox, "spinBox_5")
         cls.button = window.findChild(QtWidgets.QPushButton, "pushButton")
 
-        cls.button.clicked.connect(cls.handler)
+        cls.button.clicked.connect(cls.clicked)
         window.show()
         sys.exit(app.exec_())
 
     @classmethod
-    def handler(cls, value):
+    def clicked(cls, value):
         cls.cmdPub.publish((cls.spinbox1.value(), cls.spinbox2.value(), cls.spinbox3.value(), cls.spinbox4.value(), cls.spinbox5.value()))
 
     @classmethod
-    def callback(cls, data):
+    def report(cls, data):
         # rospy.loginfo(rospy.get_caller_id() + " joint angles deg: %s", data.position)
         cls.textbox.setText("%d\t %d\t %d\t %d\t %d" % (data.position[0], data.position[1], data.position[2], data.position[3], data.position[4]))
 
@@ -57,6 +58,16 @@ class Panel():
             cls.state.position[i] = numpy.pi*float(data.position[i])/180
 
         cls.statePub.publish(cls.state)
+
+    @classmethod
+    def execute(cls, data):
+        command = [0,numpy.pi/2,0,0,0]
+        for i in range(len(data.position)):
+            command[i] = int(180*data.position[i]/numpy.pi)
+
+        print(command)
+        cls.cmdPub.publish(command)
+
 
 if __name__ == '__main__':
     Panel()
